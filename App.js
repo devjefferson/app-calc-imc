888ca52bc7add2159fb251b6f7600d5c8bba40d3
devjefferson <40128030+devjefferson@users.noreply.github.com>
2020-05-13 16:08:38 -0300

    Test Commit
    Signed-off-by: devjefferson <40128030+devjefferson@users.noreply.github.com>

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
